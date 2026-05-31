91062fe1a626b9a813dc0cdd11823af2162f18f4
naresh-mishra <nareshmishra8535@gmail.com>
2026-05-31 18:30:21 +0530

    live-url and ss added

--- terraform/frontend_userdata.sh
+++ terraform/frontend_userdata.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+set -e
+
 apt update -y
-apt install -y nodejs npm git
+apt install -y git curl
+
+# Install Node.js 20 (IMPORTANT FIX)
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+apt install -y nodejs
+
+# Verify
+node -v
+npm -v
 
 cd /home/ubuntu
 
-git clone https://github.com/naresh-mishra/task2-separateEC2.git
+git clone https://github.com/naresh-mishra/task2-separateEC2.git || true
 
 cd task2-separateEC2/frontend
 
 npm install
 
+# Run in background
 nohup node server.js > output.log 2>&1 &
